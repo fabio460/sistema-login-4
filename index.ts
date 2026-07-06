341bc7e4922fa46789a9ae11e2af0202fec415f8
Fabio <dasilvadeoliveirafabio@email.com>
2026-07-06 17:25:53 -0300

    teste 14

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 import express from 'express';
-import  cors from "cors";
+//import  cors from "cors";
 import { getUsuario } from './controllers/usuarioController.js';
 
 const app = express();
-app.use(cors())
+//app.use(cors())
 
 app.use(express.json());
 
